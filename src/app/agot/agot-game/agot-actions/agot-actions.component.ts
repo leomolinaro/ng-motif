2cd10577daac4ef928f4039be40bd93658b4fa0a
leomolinaro <leomolinaro.motif@gmail.com>
2019-08-14 16:28:48 +0200

    .

--- src/app/agot/agot-game/agot-actions/agot-actions.component.ts
+++ src/app/agot/agot-game/agot-actions/agot-actions.component.ts
@@ -3,7 +3,7 @@ import { AgotState } from '../../store/agot.reducer';
 import { Action } from '@ngrx/store';
 import { FormGroup, FormControl } from '@angular/forms';
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
-import { InitState } from '../../store/agot.actions';
+import { InitGame } from '../../store/agot.actions';
 
 @Component({
   selector: 'agot-actions',
